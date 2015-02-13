a4b442e278e959eb8e5d01694af30295ea63f476
Jonathan Howard <howard.jonathan@gmail.com>
2015-02-12 23:56:25 -0800

    beat_signature and video path are required

--- ganglion.py
+++ ganglion.py
@@ -2,9 +2,9 @@ import argparse
 from datetime import datetime
 
 parser = argparse.ArgumentParser(description="processes audio time signatures to output a video with cuts that correspond to the times provided")
-parser.add_argument('-i', metavar='path_beat',
+parser.add_argument('-i', metavar='path_beat', required=True,
     help='relative or abs path to beat signature CSV')
-parser.add_argument('-v', metavar='path_video',
+parser.add_argument('-v', metavar='path_video', required=True,
     help='relative or abs path to video file')
 parser.add_argument('-s', metavar='sampling_rate',
     help='Percent (out of 100) of beats from which to randomly sample')
